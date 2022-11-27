3e1e6f71034b756e9b1eb8f8a65a41e3c5dd0d46
imzxj <362896731@qq.com>
2022-11-27 16:59:17 +0800

    add examples

---
+++ examples/src/App.tsx
@@ -0,0 +1,24 @@
+import './App.css'
+import { useModel } from 'rsmwr'
+import { counter } from './models/counter'
+
+function App() {
+  const { count } = useModel(counter, s => ({ count: s.count }))
+  // or
+  // useModel(counter, ['count'])
+
+  return (
+    <div className="App">
+      <p>count is: {count}</p>
+      <div className="actions">
+        <button onClick={() => counter.increase()}>increate</button>
+        <button onClick={() => counter.decrease()}>decreate</button>
+        <button onClick={() => counter.increaseAsync()}>increate (async)</button>
+        <button onClick={() => counter.decreaseAsync()}>decreate (async)</button>
+        <button onClick={() => counter.reset()}>reset</button>
+      </div>
+    </div>
+  )
+}
+
+export default App
