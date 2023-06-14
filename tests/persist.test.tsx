e40c709079f0377ca2fcc028ca7a9a65347e0ebe
imzxj <362896731@qq.com>
2023-06-14 10:29:38 +0800

    fix tests

--- tests/persist.test.tsx
+++ tests/persist.test.tsx
@@ -2,6 +2,9 @@ import { expect, test } from 'vitest'
 import { Store, Plugin } from 'zhuangtai'
 import { persist } from 'zhuangtai/plugins'
 
+const serialize = JSON.stringify
+const deserialize = JSON.parse
+
 const DUMMY_STATE_KEY = 'DUMMY_STATE'
 const map = new Map<string, string>()
 
@@ -12,56 +15,77 @@ const dummyStorage = {
   clear: () => map.clear(),
 }
 
-class MyStore extends Store<{ foo: number; bar: string }> {
+class Counter extends Store<{ count: number; foo?: string; bar?: boolean }> {
   constructor(plugins: Plugin[]) {
-    super({ foo: 1, bar: '2' }, { plugins })
+    super({ count: 0 }, { plugins })
   }
 }
 
 test('can merge state from dummy storage', () => {
   dummyStorage.clear()
-  dummyStorage.setItem(DUMMY_STATE_KEY, '{"bar":"hello"}')
-  const plugin = persist<MyStore>({
+  dummyStorage.setItem(DUMMY_STATE_KEY, serialize({ version: 0, state: { count: 100 } }))
+  const plugin = persist<Counter>({
     name: DUMMY_STATE_KEY,
     getStorage: () => dummyStorage,
   })
-  const store = new MyStore([plugin])
-  expect(store.state).toEqual({ foo: 1, bar: 'hello' })
+  const store = new Counter([plugin])
+  expect(store.state).toEqual({ count: 100 })
+})
+
+test('if version different, persisted state should ignored', () => {
+  dummyStorage.clear()
+  dummyStorage.setItem(DUMMY_STATE_KEY, serialize({ version: 1, state: { count: 100 } }))
+  const plugin = persist<Counter>({
+    name: DUMMY_STATE_KEY,
+    getStorage: () => dummyStorage,
+    version: 2,
+  })
+  const store = new Counter([plugin])
+  expect(store.state.count).toEqual(0)
 })
 
 test('can persist state to dummy storage', () => {
   dummyStorage.clear()
-  const plugin = persist<MyStore>({
+  const plugin = persist<Counter>({
     name: DUMMY_STATE_KEY,
     getStorage: () => dummyStorage,
   })
-  const store = new MyStore([plugin])
-  store.setState({ foo: 10 })
-  expect(dummyStorage.getItem(DUMMY_STATE_KEY)).toEqual('{"foo":10,"bar":"2"}')
+  const store = new Counter([plugin])
+  store.setState({ count: 100 })
+  expect(deserialize(dummyStorage.getItem(DUMMY_STATE_KEY)!)).toEqual({
+    version: 0,
+    state: { count: 100 },
+  })
 })
 
 test('can persist specified state', () => {
   dummyStorage.clear()
-  const plugin = persist<MyStore>({
+  const plugin = persist<Counter>({
     name: DUMMY_STATE_KEY,
     getStorage: () => dummyStorage,
-    partialize: s => ({ foo: s.foo }),
+    partialize: s => ({ count: s.count }),
   })
-  const store = new MyStore([plugin])
-  store.setState({ foo: 21 })
-  expect(dummyStorage.getItem(DUMMY_STATE_KEY)).toEqual('{"foo":21}')
+  const store = new Counter([plugin])
+  store.setState({ count: 100, foo: 'balabala' })
+  expect(deserialize(dummyStorage.getItem(DUMMY_STATE_KEY)!)).toEqual({
+    version: 0,
+    state: { count: 100 },
+  })
 })
 
 test('can custom serializer and deserializer', () => {
   dummyStorage.clear()
-  const plugin = persist<MyStore>({
+  dummyStorage.setItem(DUMMY_STATE_KEY, serialize({ version: 0, state: { count: 100 } }))
+  const plugin = persist<Counter>({
     name: DUMMY_STATE_KEY,
     getStorage: () => dummyStorage,
-    serialize: () => '{"bar":"asdf"}',
-    deserialize: () => ({ bar: 'qwer' }),
+    serialize: () => serialize({ version: 0, state: { count: 6 } }),
+    deserialize: () => ({ version: 0, state: { count: 9 } }),
   })
-  const store = new MyStore([plugin])
-  expect(store.state.bar).toEqual('qwer')
-  store.setState({ bar: 'alksjdfl;a;sdf' })
-  expect(dummyStorage.getItem(DUMMY_STATE_KEY)).toEqual('{"bar":"asdf"}')
+  const store = new Counter([plugin])
+  expect(store.state.count).toEqual(9)
+  store.setState({ count: 100 })
+  expect(dummyStorage.getItem(DUMMY_STATE_KEY)).toEqual(
+    serialize({ version: 0, state: { count: 6 } }),
+  )
 })
